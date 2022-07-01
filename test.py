b6d00c56f886ded531a9f27e8de6529403375bdc
ZHANGXINGYU <43384183+zxypro1@users.noreply.github.com>
2022-07-01 23:46:51 +0800

    edit

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+import tornado
 
 json_count = 0
 url = 'https://lordicon.com/api/library/icon/3418/download'
@@ -7,5 +8,6 @@ header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.30 '
 }
+img = requests.get('https://lordicon.com/api/library/icon/3418/download', headers=header)
 
-requests.get('https://lordicon.com/api/library/icon/3418/download', headers=header)
+print(img)
